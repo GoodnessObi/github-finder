53f7fa3548f48969601b425386a184e5d6c0a471
GoodnessObi <goodnessobi@gmail.com>
2021-07-03 23:17:14 +0100

    feature: build navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hiii</h1>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+      </div>
+    );
+  }
 }
+
 
 export default App;
