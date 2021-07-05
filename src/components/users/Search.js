776dfb56a8cedf2b0eb7fdf196b790d6e6d87626
GoodnessObi <goodnessobi@gmail.com>
2021-07-05 21:03:04 +0100

    feature: user can search for github user by text form handling and passing a prop up

---
+++ src/components/users/Search.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+class Search extends Component {
+  state= {
+    text: ''
+  }
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  }
+
+  // onChange = (e) => this.setState({ text: e.target.value })
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value })
+  /*first works with just one input field but with multiple onput fields, 
+  using [e.target.name] will enable the use of onechange function for multiple form fields*/
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text)
+    this.setState({ text: '' })
+  }
+
+  render() {
+    return (
+      <div>
+        <form action="" onSubmit={this.onSubmit} className="form">
+          <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange} />
+          <input type="submit" value="search" className="btn btn-dark btn-block" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Search
