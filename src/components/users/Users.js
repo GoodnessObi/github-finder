8fa5c475bddb26bb7851627060d192996d5a4b3e
GoodnessObi <goodnessobi@gmail.com>
2021-07-05 14:14:49 +0100

    move state magement up to global app level, make axios call to get data pass the app state as a prop to a component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,33 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: "ifatoki",
-        avatar_url: "https://avatars.githubusercontent.com/u/28437656?v=4",
-        html_url: "https://github.com/ifatoki"
-      },
-      {
-        id: '2',
-        login: "GoodnessObi",
-        avatar_url: "https://avatars.githubusercontent.com/u/50333058?v=4",
-        html_url: "https://github.com/GoodnessObi",
-      },
-      {
-        login: "blossom-babs",
-        id: '3',
-        avatar_url: "https://avatars.githubusercontent.com/u/58997189?v=4",
-        html_url: "https://github.com/blossom-babs",
-      }
-    ]
-  }
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ) )}
       </div>
