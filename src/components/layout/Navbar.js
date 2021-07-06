77b170f6d9777eb882d923b1671f31752998b36b
GoodnessObi <goodnessobi@gmail.com>
2021-07-06 12:07:26 +0100

    routing in react - react-router-dom, switch, route, exact paths and links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   //destructured props
@@ -10,6 +11,12 @@ const Navbar = ({ icon, title }) => {
       <h1>
         <i className={icon}></i> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </div>
   )
 }
